75e468042550761007684f5d44de83f5ee433a29
Rex Feng <rexfeng@gmail.com>
2016-02-18 11:17:48 -0800

    allow a Tree.Node to store a collection of children items

--- src/node.jl
+++ src/node.jl
@@ -1,3 +1,7 @@
 type Node
   value
+  children::Array
 end
+
+# Outer Constructor Method for convenience
+Node(v) = Node(v, Array(Node, 0))
--- test/test_node.jl
+++ test/test_node.jl
@@ -7,3 +7,16 @@ another_node = Tree.Node("different_value")
 @test another_node.value == "different_value"
 
 @test a_node.value == "some_value"
+
+# set / get Node children
+
+parent_node = Tree.Node("parent")
+child_node = Tree.Node("child_1")
+push!(parent_node.children, child_node)
+
+@test length(parent_node.children) == 1
+
+another_child_node = Tree.Node("child_2")
+push!(parent_node.children, another_child_node)
+
+@test length(parent_node.children) == 2
