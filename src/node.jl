20720ac48a18ed73cdf9d14ad367834b55f368b7
Rex Feng <rexfeng@gmail.com>
2016-02-18 14:16:54 -0800

    add Tree functions: is_ancestor, is_descendant, is_sibling for comparing nodes

--- src/node.jl
+++ src/node.jl
@@ -23,3 +23,33 @@ end
 function is_root(node::Node)
   isnull(node.parent)
 end
+
+# check if 1st node (possible_ancestor) is ancestor (older) of 2nd node (possible_descendant)
+function is_ancestor(possible_ancestor::Node, possible_descendant::Node)
+  if isnull(possible_descendant.parent)
+    return false
+  elseif get(possible_descendant.parent) == possible_ancestor
+    return true
+  else
+    is_ancestor(possible_ancestor, get(possible_descendant.parent))
+  end
+end
+
+# check if 1st node (possible_descendant) is descendant (younger) of 2nd node (possible_ancestor)
+function is_descendant(possible_descendant::Node, possible_ancestor::Node)
+  is_ancestor(possible_ancestor, possible_descendant)
+end
+
+# check if 1st node (possible_sibling) is a sibling (same parents) of 2nd node (check_node)
+function is_sibling(possible_sibling::Node, check_node::Node)
+  if isnull(possible_sibling.parent)
+    return false
+  else
+    parent = get(possible_sibling.parent)
+    if length(parent.children) <= 0
+      return false
+    else
+      return in(check_node, parent.children)
+    end
+  end
+end
--- test/test_node.jl
+++ test/test_node.jl
@@ -41,3 +41,35 @@ Tree.add_children(parent_node, [another_child_node, third_child_node])
 
 @test Tree.is_root(parent_node) == true
 @test Tree.is_root(child_node) == false
+
+# check if Node is ancestor (above/parent) of other Node
+
+grandchild_node = Tree.Node("grandchild")
+Tree.add_children(child_node, [grandchild_node])
+
+@test Tree.is_ancestor(parent_node, child_node) == true
+@test Tree.is_ancestor(parent_node, grandchild_node) == true
+@test Tree.is_ancestor(child_node, grandchild_node) == true
+
+@test Tree.is_ancestor(child_node, parent_node) == false
+@test Tree.is_ancestor(grandchild_node, parent_node) == false
+@test Tree.is_ancestor(grandchild_node, child_node) == false
+
+# check if Node is descendant (below/child) of other Node
+
+@test Tree.is_descendant(child_node, parent_node) == true
+@test Tree.is_descendant(grandchild_node, parent_node) == true
+@test Tree.is_descendant(grandchild_node, child_node) == true
+
+@test Tree.is_descendant(parent_node, child_node) == false
+@test Tree.is_descendant(parent_node, grandchild_node) == false
+@test Tree.is_descendant(child_node, grandchild_node) == false
+
+# check if Node is sibling (same parent) of other Node
+
+@test Tree.is_sibling(child_node, another_child_node) == true
+@test Tree.is_sibling(child_node, third_child_node) == true
+@test Tree.is_sibling(another_child_node, third_child_node) == true
+
+@test Tree.is_sibling(child_node, grandchild_node) == false
+@test Tree.is_sibling(parent_node, grandchild_node) == false
