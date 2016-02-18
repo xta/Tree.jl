35e1b6dff67a86685d3dad6d554d4e156afb90da
Rex Feng <rexfeng@gmail.com>
2016-02-18 13:19:44 -0800

    add Nullable parent Node reference

--- src/node.jl
+++ src/node.jl
@@ -1,7 +1,17 @@
 type Node
   value
+  parent::Nullable
   children::Array
 end
 
 # Outer Constructor Method for convenience
-Node(v) = Node(v, Array(Node, 0))
+Node(v) = Node(v, Nullable{Node}(), Array(Node, 0))
+
+# methods
+
+function add_children(parent::Node, children::Array)
+  for child in children
+    child.parent = Nullable(parent)
+    push!(parent.children, child)
+  end
+end
--- test/test_node.jl
+++ test/test_node.jl
@@ -8,15 +8,26 @@ another_node = Tree.Node("different_value")
 
 @test a_node.value == "some_value"
 
-# set / get Node children
+# set Node children & parent
 
 parent_node = Tree.Node("parent")
 child_node = Tree.Node("child_1")
-push!(parent_node.children, child_node)
+Tree.add_children(parent_node, [child_node])
 
 @test length(parent_node.children) == 1
 
 another_child_node = Tree.Node("child_2")
-push!(parent_node.children, another_child_node)
+third_child_node = Tree.Node("child_3")
+Tree.add_children(parent_node, [another_child_node, third_child_node])
 
-@test length(parent_node.children) == 2
+@test length(parent_node.children) == 3
+
+# get Node children
+
+@test parent_node.children == [child_node, another_child_node, third_child_node]
+
+# get Node parent
+
+@test get(child_node.parent) == parent_node
+@test get(another_child_node.parent) == parent_node
+@test get(third_child_node.parent) == parent_node
